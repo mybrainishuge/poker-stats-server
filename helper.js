13c23d8698b8cad2be0802b3fd543c8f5b3a31fb
Richard May <richard@italist.com>
2018-12-01 23:59:27 -0800

    use cloudinary library and dotenv

--- helper.js
+++ helper.js
@@ -1,16 +1,27 @@
-// chaning regular expressions feels a bit clunky
+const cloudinary = require('cloudinary');
+
+cloudinary.config({
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: 'viz',
+});
+
 const normalizeName = (first, last) =>
   `${first} ${last}`
     .replace(/[^a-z+]+/gi, '-')
     .replace(/(^[^a-z]+)|([^a-z]+$)/gi, '')
     .toLowerCase();
 
-// may want to get the cloudinary base url from an environment variable
 const getAvatarUrl = (first, last) =>
-  `https://res.cloudinary.com/viz/image/upload/ar_1:1,c_fill,g_auto:face,q_100,r_max,w_100/v1543707820/avatar/${normalizeName(
-    first,
-    last
-  )}.jpg`;
+  cloudinary.url(`avatar/${normalizeName(first, last)}.jpg`, {
+    aspect_ratio: '1:1',
+    crop: 'fill',
+    gravity: 'face:auto',
+    quality: 100,
+    radius: 'max',
+    secure: true,
+    width: 50,
+  });
 
 const currency = new Intl.NumberFormat('en-US', {
   style: 'currency',
